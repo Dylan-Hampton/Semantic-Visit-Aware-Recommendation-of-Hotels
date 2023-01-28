c0ca94d494b90941fd8936f4208e300385f8e0af
Dylan H <dhampton@iastate.edu>
2023-01-28 12:55:32 -0600

    Added Skeleton Submission button

--- Frontend/svarh/src/components/App/App.tsx
+++ Frontend/svarh/src/components/App/App.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import Icon from '@mui/material/Icon';
+import HotelIcon from '@mui/icons-material/Hotel';
+
+
+const styles = {
+  submitBtn: {
+    fontSize: 45,
+  },
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button 
+          className='submitButton'
+          variant="contained"
+          startIcon=<HotelIcon style={{fontSize:45}} /> 
+          sx={styles.submitBtn}
+          onClick={() => console.log("Clicked Submit")}
+          >
+            Submit
+          </Button>
       </header>
     </div>
   );
