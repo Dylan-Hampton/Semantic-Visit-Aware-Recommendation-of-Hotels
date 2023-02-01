dde7cd2b717bd2dbe30992a5c948e454542b9dd1
nate-schenck <schenck.nate@yahoo.com>
2023-02-01 15:53:34 -0600

    Added generated route skeleton

--- Frontend/svarh/src/components/App/App.tsx
+++ Frontend/svarh/src/components/App/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import MapBase from '../map/MapBase/MapBase';
 import Settings from '../inputBar/advancedSettings/Settings/Settings';
+import GeneratedRoute from '../results/generatedRoute/GeneratedRoute/GeneratedRoute';
 
 function App() {
   return (
     <div className="App">
       <Settings />
+      <GeneratedRoute routes={[{},{},{}]}/>
       <MapBase />
   </div>
   );
---
+++ Frontend/svarh/src/components/results/generatedRoute/Route/Route.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface IRouteProps {
+    route: IRouteData;
+}
+
+export interface IRouteData {
+    
+}
+
+const Route: React.FC<IRouteProps> = (props: IRouteProps) => {
+
+    // TODO: Display the route with icons/names like in the Figma design
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Route;
