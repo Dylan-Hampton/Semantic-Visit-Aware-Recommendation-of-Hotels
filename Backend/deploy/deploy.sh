ca89004e35f1956eaa8c87fc6371cb11c281d55d
Zachary Garwood <garwoodzachary@gmail.com>
2023-02-11 14:10:03 -0600

    Specified host and port when starting app

--- Backend/deploy/deploy.sh
+++ Backend/deploy/deploy.sh
@@ -2,14 +2,14 @@
 
 
 echo "Installing dependencies"
-pip install flask
+pip install flask gunicorn
 
-echo "Deploying to ${DEPLOY_SERVER}"
-# Copy the backend files to the Backend/ directory on the server
-cp -r * /home/vm-user/sd/sdmay23-34/Backend
+# echo "Deploying to ${DEPLOY_SERVER}"
+# # Copy the backend files to the Backend/ directory on the server
+# cp -r * /home/vm-user/sd/sdmay23-34/Backend
 
-echo "Finished copying the backend files"
+# echo "Finished copying the backend files"
 
 echo "Starting flask app"
 export FLASK_APP=app.py
-flask run
+python3 -m app
