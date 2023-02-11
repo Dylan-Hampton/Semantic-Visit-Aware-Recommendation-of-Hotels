31a06cd8a4f876ea33c29c71d362fe2a62e1df93
Zachary Garwood <garwoodzachary@gmail.com>
2023-02-11 16:33:47 -0600

    Removed second gitlab runner and changed deploy script to only copy files over (need to change apache conf files on the server to use WSGI for flask)

--- Backend/deploy/deploy.sh
+++ Backend/deploy/deploy.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
+DEPLOY_SERVER=sdmay23-34.ece.iastate.edu
 
-echo "Installing dependencies"
-pip install flask gunicorn
+echo "Deploying to ${DEPLOY_SERVER}"
+cp -r * /home/vm-user/sd/sdmay23-34/Backend
 
-# echo "Deploying to ${DEPLOY_SERVER}"
-# # Copy the backend files to the Backend/ directory on the server
-# cp -r * /home/vm-user/sd/sdmay23-34/Backend
-
-# echo "Finished copying the backend files"
-
-echo "Starting flask app"
-export FLASK_APP=app.py
-gunicorn -b 127.0.0.1:80 app:app
+echo "Finished copying the backend files"
