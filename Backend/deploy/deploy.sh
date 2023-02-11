8fd39c25ed27c166196560331fe619e8bcb5d706
Zachary Garwood <garwoodzachary@gmail.com>
2023-02-11 13:45:57 -0600

    Added deployment for backend to yml config

---
+++ Backend/deploy/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+echo "Installing dependencies"
+pip install flask
+
+echo "Deploying to ${DEPLOY_SERVER}"
+# Copy the backend files to the Backend/ directory on the server
+cp -r * /home/vm-user/sd/sdmay23-34/Backend
+
+echo "Finished copying the backend files"
+
+echo "Starting flask app"
+export FLASK_APP=app.py
+flask run
