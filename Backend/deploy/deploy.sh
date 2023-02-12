1fc45416786f4f7d0f7b44729ae6e3fc3ce29112
Zachary Garwood <garwoodzachary@gmail.com>
2023-02-12 16:29:06 -0600

    Added WSGI server to VM for flask app and made a few modifications to the cicd scripts

--- Backend/deploy/deploy.sh
+++ Backend/deploy/deploy.sh
@@ -3,6 +3,9 @@
 DEPLOY_SERVER=sdmay23-34.ece.iastate.edu
 
 echo "Deploying to ${DEPLOY_SERVER}"
-cp -r * /home/vm-user/sd/sdmay23-34/Backend
-
+cp -r * /var/www/flask-app/
 echo "Finished copying the backend files"
+
+echo "Restarting apache"
+sudo systemctl reload apache2
+echo "Finished restarting apache"
