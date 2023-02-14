9bcfb5d8552ddfc5c4d9534701e77026b4836519
nschenck <nschenck@iastate.edu>
2023-02-14 12:35:28 -0600

    Changed GR return button text to "View Hotels"

--- Frontend/svarh/src/components/results/generatedRoute/ReturnButton/ReturnButton.tsx
+++ Frontend/svarh/src/components/results/generatedRoute/ReturnButton/ReturnButton.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './ReturnButton.css';
-import ReplyAllIcon from '@mui/icons-material/ReplyAll';
+import ReplyIcon from '@mui/icons-material/Reply';
 import { Grid } from "@mui/material";
 
 interface IReturnButtonProps {
@@ -9,11 +9,11 @@ interface IReturnButtonProps {
 
 const ReturnButton: React.FC<IReturnButtonProps> = (props: IReturnButtonProps) => {
     return (
-        <Grid container className="return-container">
-            <Grid item xs={4} justifyItems="center" justifyContent="center" className="return-icon">
-                <ReplyAllIcon />
+        <Grid container className="return-container" spacing={1}>
+            <Grid item xs={2} justifyItems="center" justifyContent="center" className="return-icon">
+                <ReplyIcon />
             </Grid>
-            <Grid item xs={8} justifyItems="center" justifyContent="center" className="return-text">Return</Grid>
+            <Grid item xs={10} justifyItems="center" justifyContent="center" className="return-text" >View Hotels</Grid>
         </Grid>
     )
 }
