a19f4a64a8d4466b3b9b1a042434e261911185ac
Dylan H <dhampton@iastate.edu>
2023-02-20 16:40:07 -0600

    Added header + submit button tests

--- Frontend/svarh/src/components/App/App.test.tsx
+++ Frontend/svarh/src/components/App/App.test.tsx
@@ -2,8 +2,26 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+export const mockMapOn = jest.fn();
+export const mockMapRemove = jest.fn();
+
+jest.mock('mapbox-gl', () => ({
+    Map: function () {
+      // @ts-ignore
+      this.on = mockMapOn;
+      // @ts-ignore
+      this.remove = mockMapRemove;
+    }
+  }));
+
+test('renders search frame header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkHeader = screen.getByText(/Search Hotels/i);
+  expect(linkHeader).toBeInTheDocument();
+});
+
+test('renders submission button', () => {
+  render(<App />);
+  const submitBtn = screen.getByText(/Submit/i);
+  expect(submitBtn).toBeInTheDocument();
 });
