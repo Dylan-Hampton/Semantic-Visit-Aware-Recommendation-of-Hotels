49c9838f8daf67f18c0a0a1edadf59454d1a1242
nate-schenck <schenck.nate@yahoo.com>
2023-02-20 16:15:53 -0600

    Worker Loader now only runs in non-test env

--- Frontend/svarh/src/components/map/MapBase/MapBase.tsx
+++ Frontend/svarh/src/components/map/MapBase/MapBase.tsx
@@ -4,9 +4,12 @@ import './MapBase.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 mapboxgl.accessToken = "pk.eyJ1IjoibmF0ZXNjaGVuY2siLCJhIjoiY2xkZ2hha3IwMHJ6djN3bndlYzlud29vaSJ9.4gjvZipOtY9lWJXc3Ffk6g";
-// @ts-ignore
-// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
-mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+if (process.env.NODE_ENV !== 'test'){
+    // @ts-ignore
+    // eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+    mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+}
+
 
 interface IMapBaseProps {
 
