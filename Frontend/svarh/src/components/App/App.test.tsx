dc845dadabe944dea322adb6a0c3018351e907fa
Dylan-Hampton <dhampt190@gmail.com>
2023-02-21 11:36:12 -0600

    Added tests for algorithm settings menu

--- Frontend/svarh/src/components/App/App.test.tsx
+++ Frontend/svarh/src/components/App/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 export const mockMapOn = jest.fn();
@@ -16,8 +16,7 @@ jest.mock('mapbox-gl', () => ({
 
 test('renders search frame header', () => {
   render(<App />);
-  const linkHeader = screen.getByText(/Search Hotels/i);
-  expect(linkHeader).toBeInTheDocument();
+  expect(screen.getByText(/Search Hotels/i)).toBeInTheDocument();
 });
 
 test('renders submission frame inputs', () => {
@@ -29,6 +28,40 @@ test('renders submission frame inputs', () => {
 
 test('renders submission button', () => {
   render(<App />);
-  const submitBtn = screen.getByRole("button", { name: "Submit" });
-  expect(submitBtn).toBeInTheDocument();
+  expect(screen.getByRole("button", { name: "Submit" })).toBeInTheDocument();
+  expect(screen.getByRole("button", { name: "Submit" })).toBeEnabled();
+});
+
+test('renders settings button', () => {
+  render(<App />);
+  expect(screen.getByTestId("SettingsIcon")).toBeInTheDocument(); 
+});
+
+test('settings button handles click', () => {
+  render(<App />);
+  const settingsBtn = screen.getByTestId("SettingsIcon");
+  expect(settingsBtn).toBeInTheDocument(); 
+  fireEvent.click(settingsBtn);
+  expect(settingsBtn).not.toBeInTheDocument();
+  expect(screen.getByLabelText("CloseSettingsMenuBtn")).toBeInTheDocument();
+  expect(screen.getByText("Advanced Settings")).toBeInTheDocument();
+  expect(screen.getByLabelText("Algorithm")).toBeInTheDocument();
+});
+
+test('open and close settings panel', () => {
+  render(<App />);
+  const settingsBtn = screen.getByTestId("SettingsIcon");
+  fireEvent.click(settingsBtn);
+  expect(settingsBtn).not.toBeInTheDocument();
+  const closeSettingsBtn = screen.getByLabelText("CloseSettingsMenuBtn");
+  const settingsTxt = screen.getByText("Advanced Settings");
+  const algoDropdown = screen.getByLabelText("Algorithm");
+  expect(closeSettingsBtn).toBeInTheDocument();
+  expect(settingsTxt).toBeInTheDocument();
+  expect(algoDropdown).toBeInTheDocument();
+  fireEvent.click(screen.getByLabelText("CloseSettingsMenuBtn"));
+  expect(screen.getByTestId("SettingsIcon")).toBeInTheDocument(); 
+  expect(closeSettingsBtn).not.toBeInTheDocument();
+  expect(settingsTxt).not.toBeInTheDocument();
+  expect(algoDropdown).not.toBeInTheDocument();
 });
