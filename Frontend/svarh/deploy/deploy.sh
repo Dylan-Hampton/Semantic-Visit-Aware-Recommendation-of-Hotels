ef82c6e452ea84dcea120bdf2c9b1d36d6a96c3f
nate-schenck <schenck.nate@yahoo.com>
2023-02-21 22:48:49 -0600

    Attempt to fix CD

--- Frontend/svarh/deploy/deploy.sh
+++ Frontend/svarh/deploy/deploy.sh
@@ -8,7 +8,7 @@ npm install
 npm run build
 
 echo "Deploying to ${DEPLOY_SERVER}"
-sudo mkdir /var/www/sdmay23-34.ece.iastate.edu
-sudo cp -r build/* /var/www/sdmay23-34.ece.iastate.edu/
+mkdir /var/www/sdmay23-34.ece.iastate.edu
+cp -r build/* /var/www/sdmay23-34.ece.iastate.edu/
 
 echo "Finished copying the build files"
