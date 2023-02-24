99973209be9bfe536db36128c91fd5d99cd5ef1d
Zachary Garwood <garwoodzachary@gmail.com>
2023-02-24 16:44:25 -0600

    Added functions to format the route result into a JSON

--- Backend/app.py
+++ Backend/app.py
@@ -268,16 +268,53 @@ def routes():
 
     theta = [0,1,0,1,0,1]
     max_dist = 2000
-    num_required_origin = 5
+    num_required_origin = 1
     max_time = 20
 
     alg_poi_res = GreedySearch.greedy_process_PoI(g, container_index, theta, max_dist, origins, num_required_origin, index_matrix=True, verbal=False, complexity=False)
-    
-    return jsonify(alg_poi_res)
-
     # gd_res = greedy_dijkstra(g, origins, theta, max_dist, num_required_origin, verbal=False, complexity=False)
     # alg_o_res = GreedySearch.greedy_process_origin(g, container_index, theta, max_dist, origins, num_required_origin, index_matrix=True, verbal=False, complexity=False)
     # rwr_res = random_walk_restart(g, origins, theta, max_dist, 3*max_time, num_required_origin, verbal=False, complexity=False)
+
+    return jsonify(get_result_JSON(g, alg_poi_res))
+
+def get_result_JSON(g, route_res):
+    result = []
+    path_JSON = {'origin': [], 'nodes': [], 'pois': [], 'distance': ''}
+
+    # Iterate through each route (there can be multiple origins)
+    for data in route_res:
+        # Get node id's and poi info
+        origin_id = data[0]
+        path = data[1]
+        distance = data[2]
+        pois = data[3]
+
+        # Construct result JSON for a certain origin
+        path_JSON['origin'] = get_node_JSON(g.nodes[origin_id])
+
+        for node_id in path:
+            path_JSON['nodes'].append(get_node_JSON(g.nodes[node_id]))
+
+        path_JSON['distance'] = distance
+
+        for poi in pois:
+            path_JSON['pois'].append(get_poi_JSON(g, path, poi))
+        
+        result.append(path_JSON)
+    return result
+
+def get_node_JSON(node):
+        return {'lng': node.lng, 'lat': node.lat}
+
+# TODO: Find a way to not have to iterate through all nodes in the path to find PoI's lat,long
+def get_poi_JSON(g, path, poi):
+    # PoIs don't have lat,long attributes in their class, so we need to iterate through each node in the path to find the PoI's lat,long
+    for node_id in path:
+        node = g.nodes[node_id]
+        if poi in node.PoIs:
+            return {'name': poi.name, 'category': poi.category, 'lng': node.lng, 'lat': node.lat}
+
 
 if __name__ == '__main__':
     app.run()
