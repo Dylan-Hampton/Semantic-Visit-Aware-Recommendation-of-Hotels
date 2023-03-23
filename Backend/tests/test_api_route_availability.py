d8e431c4fb8b5dc1ebe30716ed5a6be6c48a99ab
Zachary Garwood <garwoodzachary@gmail.com>
2023-03-22 19:20:28 -0500

    Changed test route and yml

--- Backend/tests/test_api_route_availability.py
+++ Backend/tests/test_api_route_availability.py
@@ -1,7 +1,7 @@
 import pytest
 from app import app
 
-def test_index_route():
-    response = app.test_client().get('/')
+def test_cities_route():
+    response = app.test_client().get('/cities')
 
     assert response.status_code == 200
