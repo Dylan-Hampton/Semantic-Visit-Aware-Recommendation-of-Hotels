0a3375294236cda7ac3fe19295a5b74743e504b0
Zachary Garwood <garwoodzachary@gmail.com>
2023-03-22 19:40:18 -0500

    Added request body to routes test and fix small issue in app.py

--- Backend/tests/test_api_route_availability.py
+++ Backend/tests/test_api_route_availability.py
@@ -5,3 +5,13 @@ def test_cities_route():
     response = app.test_client().get('/cities')
 
     assert response.status_code == 200
+
+def test_routes_route():
+    response = app.test_client().get('/routes', json={
+        "algorithm": 3,
+        "origins": 1,
+        "distance": 2000,
+        "categories": [0,1,0,1,0,1]
+    })
+
+    assert response.status_code == 200
