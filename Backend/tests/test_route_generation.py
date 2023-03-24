deb8effb9ac88a585917eacb7fabb916e499e413
Zachary Garwood <garwoodzachary@gmail.com>
2023-03-23 21:30:11 -0500

    Added PoI first route generation test

--- Backend/tests/test_route_generation.py
+++ Backend/tests/test_route_generation.py
@@ -7,7 +7,7 @@ POI_FIRST = 2
 ORIGIN_FIRST = 3
 
 def test_greedy_dijkstra_generation():
-    
+
     # 1 origin, 1 PoI of first category
     response = app.test_client().get('/routes', json={
         "algorithm": GREEDY_DIJKSTRA,
@@ -93,4 +93,45 @@ def test_random_walk_restart_generation():
                 poi_categories.add(response.json[origin_idx]["pois"][poi_idx]["category"])
             # There should be 6 PoI categories
             assert len(poi_categories) == 6
+
+def test_poi_first_generation():
+    
+    # 1 origin, 1 PoI of first category
+    response = app.test_client().get('/routes', json={
+        "algorithm": POI_FIRST,
+        "origins": 1,
+        "distance": 1000,
+        "categories": [1,0,0,0,0,0]
+    })
+
+    assert response.status_code == 200
+    assert response.json[0]["distance"] == 276.72653132359375
+    assert len(response.json[0]["nodes"]) == 4
+    assert response.json[0]["origin"]["name"] == "Seaport Inn"
+    assert response.json[0]["pois"][0]["category"] == 0
+    assert response.json[0]["pois"][0]["name"] == "St. Patrick's Cathedral"
+
+    # 3 origins, 1 PoI of each category
+    response = app.test_client().get('/routes', json={
+        "algorithm": POI_FIRST,
+        "origins": 3,
+        "distance": 2000,
+        "categories": [1,1,1,1,1,1]
+    })
+    
+    # Generation of routes does not seem to be deterministic (PoIs change on different runs)
+    # Testing lengths of responses to account for above
+    assert response.status_code == 200
+    for origin_idx in range(3):
+        # Make sure a path is generated and has an origin with a name
+        assert response.json[origin_idx]["distance"] is not None
+        assert response.json[origin_idx]["origin"]["name"] is not None 
+        
+        poi_categories = set()
+        for poi_idx in range(6):
+            assert response.json[origin_idx]["pois"][poi_idx]["name"] is not None
+            # Add category number to set
+            poi_categories.add(response.json[origin_idx]["pois"][poi_idx]["category"])
+        # There should be 6 PoI categories
+        assert len(poi_categories) == 6
 
