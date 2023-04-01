da8b27b37c0ee32f64a6105adbe30effe7127432
nate-schenck <schenck.nate@yahoo.com>
2023-04-01 14:11:17 -0500

    Selecting a hotel from list draws route

--- Frontend/svarh/src/components/results/recommendedHotels/HotelListItem/HotelListItem.tsx
+++ Frontend/svarh/src/components/results/recommendedHotels/HotelListItem/HotelListItem.tsx
@@ -2,6 +2,9 @@ import { Grid } from '@mui/material';
 import React from 'react';
 import HotelIcon from '@mui/icons-material/Hotel';
 import './HotelListItem.css';
+import { useAppSelector } from '../../../../hooks';
+import { IMarkerWithName, selectMarkers, selectToggleRoute } from '../../../../mapDataSlice';
+import { IMarkerData } from '../../../map/MapBase/MapBase';
 
 interface IHotelListItemProps {
     name: string;
@@ -9,9 +12,30 @@ interface IHotelListItemProps {
 }
 
 const HotelListItem: React.FC<IHotelListItemProps> = (props: IHotelListItemProps) => {
+    const markers: IMarkerWithName[] = useAppSelector(selectMarkers);
+    const toggleRoute = useAppSelector(selectToggleRoute);
+
+    const drawRoute = () => {
+        const data: IMarkerData = {
+            name: props.name,
+            lat: 0,
+            lng: 0,
+            type: 'origin'
+        }
+        toggleRoute(data);
+    }
+
+    const togglePopup = () => {
+        markers.forEach((m) => {
+            if (m.name === props.name) {
+                m.marker.togglePopup();
+                return;
+            }
+        });
+    }
     return (
         <>
-            <Grid container display="flex" className="list-item">
+            <Grid container display="flex" className="list-item" onMouseEnter={togglePopup} onMouseLeave={togglePopup} onClick={drawRoute}>
                 <Grid item xs={2} display="flex" justifyContent="center" alignItems="center" ><HotelIcon /></Grid>
                 <Grid item xs={10} container direction="column">
                     <Grid item className="hotel-name">{props.name}</Grid>
