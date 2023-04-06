430b2fc0f20b08d872e3d6ac6302ad7eff572fa3
britneyyu <britneyyu3@gmail.com>
2023-04-05 21:55:49 -0500

    updates to app file

---
+++ Frontend/svarh/src/components/results/recommendedHotels/Route/RouteItem.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { PoiNode } from "../../../../data/response/Node";
+import './RouteItem.css';
+import { Grid } from "@mui/material";
+import { getIcon } from "../../../../data/IconMap";
+import { City } from "../../../../data/City";
+import { useAppSelector } from "../../../../hooks";
+import { selectCity } from "../../../../routeDataSlice";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+interface IRouteProps {
+    route: IRouteData;
+}
+
+export interface IRouteData {
+    pois: PoiNode[];
+}
+
+const RouteItem: React.FC<IRouteProps> = (props: IRouteProps) => {
+    const city: City = useAppSelector(selectCity);
+    // TODO: Display the route with icons/names like in the Figma design
+
+    return (
+        <>
+            <Grid container display="flex" className="route-item">
+                {props.route.pois.map((poi, i) => {
+                    return (
+                        <div key={i} className="poi-key">
+                        <div className="poi">
+                        <Grid item xs={2} display="flex" justifyContent="center" alignItems="center" >
+                            {getIcon(city.poiTypes[poi.category])}
+                        </Grid>
+                        <Grid item xs={10} container direction="column">
+                            <Grid item className="poi-name">{poi.name}</Grid>
+                            {city !== undefined && city.poiTypes !== undefined && city.poiTypes[poi.category] !== undefined &&
+                                <Grid item className="poi-category">
+                                    Category: {city.poiTypes[poi.category]}
+                                </Grid>
+                            }
+                        </Grid>
+                        </div>
+                        {i !== props.route.pois.length - 1 && 
+                            <Grid item xs={2} display="flex" justifyContent="center" alignItems="center">  
+                                <MoreVertIcon />
+                            </Grid>
+                        }
+                        </div>
+                    )
+                })}
+            </Grid>
+        </>
+    )
+}
+
+export default RouteItem;
