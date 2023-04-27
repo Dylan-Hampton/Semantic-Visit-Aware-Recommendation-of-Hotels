01b20e8889482a0c2bc670605a8e3a515a1ae9f2
britneyyu <britneyyu3@gmail.com>
2023-04-27 16:34:07 -0500

    poi categories

--- Backend/tests/test_poi_categories.py
+++ Backend/tests/test_poi_categories.py
@@ -4,84 +4,78 @@ import map_package.CONSTANTS as CONSTANTS
 
 
 def test_origin_first_generation_poi1():
-    # 1 origin, 1 PoI of first category
+    # origins 3, 1 PoI of first category, MEMORIAL
     response = app.test_client().post('/routes',
                                       json={
-                                          "algorithm": CONSTANTS.ORIGIN_FIRST,
+                                          "algorithm": 3,
                                           "origins": 3,
-                                          "distance": 10000,
-                                          "categories": [1, 0, 0, 0, 0, 0],
-                                          "city": "Chicago"
+                                          "distance": 1000,
+                                          "categories": [2, 0, 0, 0, 0, 0],
+                                          "city": "New York City"
                                       })
 
-    assert response.json[0]["pois"][0]["category"] == 1
+    assert response.json[0]["pois"][0]["category"] == 0
 
     def test_origin_first_generation_poi2():
-        # 1 origin, 1 PoI of first category
+        # origins 3, 1 PoI of first category, ATTRACTION
         response = app.test_client().post('/routes',
                                           json={
-                                              "algorithm":
-                                              CONSTANTS.ORIGIN_FIRST,
+                                              "algorithm": 3,
                                               "origins": 3,
-                                              "distance": 10000,
-                                              "categories": [0, 1, 0, 0, 0, 0],
-                                              "city": "Chicago"
+                                              "distance": 1000,
+                                              "categories": [0, 2, 0, 0, 0, 0],
+                                              "city": "New York City"
+                                          })
+
+        assert response.json[0]["pois"][0]["category"] == 1
+
+    def test_origin_first_generation_poi3():
+        # origins 3, 1 PoI of first category, SHOPPING
+        response = app.test_client().post('/routes',
+                                          json={
+                                              "algorithm": 3,
+                                              "origins": 3,
+                                              "distance": 1000,
+                                              "categories": [0, 0, 1, 0, 0, 0],
+                                              "city": "New York City"
                                           })
 
         assert response.json[0]["pois"][0]["category"] == 2
 
-    def test_origin_first_generation_poi3():
-        # 1 origin, 1 PoI of first category
+    def test_origin_first_generation_poi4():
+        # origins 3, 1 PoI of first category, ENTERTAINMENT
         response = app.test_client().post('/routes',
                                           json={
-                                              "algorithm":
-                                              CONSTANTS.ORIGIN_FIRST,
+                                              "algorithm": 3,
                                               "origins": 3,
-                                              "distance": 10000,
-                                              "categories": [0, 0, 1, 0, 0, 0],
-                                              "city": "Chicago"
+                                              "distance": 1000,
+                                              "categories": [0, 0, 0, 1, 0, 0],
+                                              "city": "New York City"
                                           })
 
         assert response.json[0]["pois"][0]["category"] == 3
 
-    def test_origin_first_generation_poi4():
-        # 1 origin, 1 PoI of first category
+    def test_origin_first_generation_poi5():
+        # origins 3, 1 PoI of first category, MUSEUM
         response = app.test_client().post('/routes',
                                           json={
-                                              "algorithm":
-                                              CONSTANTS.ORIGIN_FIRST,
+                                              "algorithm": 3,
                                               "origins": 3,
-                                              "distance": 10000,
-                                              "categories": [0, 0, 0, 1, 0, 0],
-                                              "city": "Chicago"
+                                              "distance": 1000,
+                                              "categories": [0, 0, 0, 0, 1, 0],
+                                              "city": "New York City"
                                           })
 
         assert response.json[0]["pois"][0]["category"] == 4
 
     def test_origin_first_generation_poi5():
-        # 1 origin, 1 PoI of first category
+        # origins 3, 1 PoI of first category, PARK
         response = app.test_client().post('/routes',
                                           json={
-                                              "algorithm":
-                                              CONSTANTS.ORIGIN_FIRST,
+                                              "algorithm": 3,
                                               "origins": 3,
-                                              "distance": 10000,
-                                              "categories": [0, 0, 0, 0, 1, 0],
-                                              "city": "Chicago"
+                                              "distance": 1000,
+                                              "categories": [0, 0, 0, 0, 0, 1],
+                                              "city": "New York City"
                                           })
-
         assert response.json[0]["pois"][0]["category"] == 5
-
-    def test_origin_first_generation_poi5():
-        # 1 origin, 1 PoI of first category
-        response = app.test_client().post('/routes',
-                                          json={
-                                              "algorithm":
-                                              CONSTANTS.ORIGIN_FIRST,
-                                              "origins": 3,
-                                              "distance": 10000,
-                                              "categories": [0, 0, 0, 0, 1, 0],
-                                              "city": "Chicago"
-                                          })
-
-        assert response.json[0]["pois"][0]["category"] == 6
